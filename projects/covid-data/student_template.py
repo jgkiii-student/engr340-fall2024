7248981e52e6df46b31443f6b2ae8f432338e633
jgkiii <kogonjg@dukes.jmu.edu>
2024-10-24 11:44:44 -0400

    commit 10.24

--- projects/covid-data/student_template.py
+++ projects/covid-data/student_template.py
@@ -69,17 +69,18 @@ def first_question(data):
     """
 
     # your code here
+    # create new dataframe from provided data
     df = pd.DataFrame(data, columns=['date','county','state','cases','deaths'])
 
-    df_RC = df[df['county'] == 'Rockingham' and df['cases'] == 1]
-    df_HB = df[df['county'] == 'Harrisonburg' and df['cases'] == 1]
+    # isolate rockingham/harrisonburg city, virginia, and when cases are greater than or equal to 1
+    df_RC = df[(df['county'] == 'Rockingham') & (df['state'] == 'Virginia') & (df['cases'] >= 1)]
+    df_HB = df[(df['county'] == 'Harrisonburg city') & (df['state'] == 'Virginia') & (df['cases'] >= 1)]
 
-    array_RC = df_RC.to_numpy()
-    array_HB =df_HB.to_numpy()
+    # find the date of the first appearance of a covid case as from previously sorted dataframe
+    RC_COVID_Date = df_RC.iloc[0]['date']
+    HB_COVID_Date = df_HB.iloc[0]['date']
 
-    RC_COVID_Date = array_RC[0,0]
-    HB_COVID_Date = array_HB[0,0]
-
+    # print results
     print(f"The first positive COVID case in Rockingham County was on {RC_COVID_Date}")
     print(f"The first positive COVID case in Harrisonburg was on {HB_COVID_Date}")
 
@@ -93,13 +94,60 @@ def second_question(data):
     :return:
     """
 
-    # your code here
+    # create new dataframe from provided data
+    df = pd.DataFrame(data, columns=['date', 'county', 'state', 'cases', 'deaths'])
+
+    # isolate rockingham/harrisonburg city and virginia dataframe data
+    df_RC = df[(df['county'] == 'Rockingham') & (df['state'] == 'Virginia')]
+    df_HB = df[(df['county'] == 'Harrisonburg city') & (df['state'] == 'Virginia')]
+
+    # find the differences between each datapoint within the 'cases' column of data and take absolute value
+    difference_RC = df_RC['cases'].diff().abs()
+    difference_HB = df_HB['cases'].diff().abs()
+
+    # find the index of the maximum value within the previously found datapoints
+    index_RC_cases = difference_RC.idxmax()
+    index_HB_cases = difference_HB.idxmax()
+
+    # find the date in which there was the greatest number of new daily cases
+    jump_date_RC = df_RC.loc[index_RC_cases, 'date']
+    jump_date_HB = df_HB.loc[index_HB_cases, 'date']
+
+    # print results
+    print(f"the greatest number of new daily cases recorded in Rockingham County was on {jump_date_RC}")
+    print(f"the greatest number of new daily cases recorded in Harrisonburg was on {jump_date_HB}")
+
     return
 
 def third_question(data):
     # Write code to address the following question:Use print() to display your responses.
     # What was the worst 7-day period in either the city and county for new COVID cases?
     # This is the 7-day period where the number of new cases was maximal.
+
+    # create new dataframe from provided data
+    df = pd.DataFrame(data, columns=['date', 'county', 'state', 'cases', 'deaths'])
+
+    # isolate rockingham/harrisonburg city and virginia dataframe data
+    df_RC = df[(df['county'] == 'Rockingham') & (df['state'] == 'Virginia')]
+    df_HB = df[(df['county'] == 'Harrisonburg city') & (df['state'] == 'Virginia')]
+
+    # find the differences between each period of 7 days within the 'cases' column of data
+    difference_RC = df_RC['cases'].diff(periods=6).abs()
+    difference_HB = df_HB['cases'].diff(periods=6).abs()
+
+    # determine which area has the worst 7-day period of new covid cases
+    if difference_RC.max() > difference_HB.max():
+        index_cases = difference_RC.idxmax()                     #finds the index of the largest difference
+        worst_period = df_RC.loc[index_cases, 'date']            #finds the initial day of the worst 7 day period w.r.t. new COVID cases
+        area = "Rockingham"                                      #sets and "area" parameter for results
+
+    else:
+        index_cases = difference_HB.idxmax()
+        worst_period = df_HB.loc[index_cases, 'date']
+        area = "Harrisonburg city"
+
+    # print results
+    print(f"The worst 7 day period of new covid cases started on {worst_period} in {area}")
 
     return
 
